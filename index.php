bc146718fa59d93621fb4d554f4e1da9ffb24f50
Samuele <miotto.samuele@gmail.com>
2022-01-17 14:37:00 +0100

    add comments

--- index.php
+++ index.php
@@ -41,10 +41,12 @@ class Film {
     public $sottotitolo;
     public $regista;
 
+    // funzione construct che accetta il titolo 
     public function __construct($titolo){
         $this -> titolo = $titolo;
     }
 
+    // se non c'è sottotitolo stampa solo il titolo, altrimenti stampa due punti e sottotitolo
     public function getFullTitle(){
         if (!$this->sottotitolo) {
             return $this->titolo;
@@ -53,6 +55,7 @@ class Film {
         }
     }
 
+    // se non c'è il regista stampa ??? altrimenti stampa il regista, per stampare titolo e sottotilo richiamo la funzione precedente
     public function __toString(){
         if (!$this->regista) {
             return $this->getFullTitle() . " | ???";
@@ -62,15 +65,19 @@ class Film {
     }
 }
 
+// creo film e do il titolo 
 $f1 = new Film("Matrix");
 
+// creo film e do il titolo, dopo assegno il sottotiolo
 $f2 = new Film("Fantozzi 2");
 $f2 -> sottotitolo = "Il ritorno di Fantozzi";
 
+// creo il film e do il titolo, assegno sottotitolo e regista
 $f3 = new Film("Peter Pan 2");
 $f3 -> sottotitolo = "Il ritorno all'isola che non c'è";
 $f3 -> regista = "Robin Budd";
 
+// stampo i tre film usando il __toString() (non è necessario specificarlo)
 echo $f1 . "<br>";
 echo $f2 . "<br>";
 echo $f3 . "<br>";
